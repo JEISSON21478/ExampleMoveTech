b7b74387a074e607e91750996ada639f5b9938c8
JJUNCOGONZA <47018595+JJUNCOGONZA@users.noreply.github.com>
2023-05-22 18:23:18 -0500

    algo

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ExampleMoveTech/', // Asegúrate de reemplazar 'ExampleMoveTech' con el nombre de tu repositorio
   plugins: [react()]
 })
+
+
